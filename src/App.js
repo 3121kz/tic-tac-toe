72baabae4a800d9a0b37b529c8c0e243ddc2b54e
3121kz <zelinsky.erik17@gmail.com>
2023-05-31 23:37:45 +0200

    Rows and collumns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import "./App.css";
 import { useState } from "react";
+import Square from "./components/Square";
+
 
 function App() {
+  const x = <img src={Cross} alt="Cross" />
+  const o = 
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState();
+  const chooseSquare = (square) => {};
+
   return (
     <div className="App">
       <div className="board">
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row"> {x}</div>
         <div className="row"></div>
       </div>
     </div>
