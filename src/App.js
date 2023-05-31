854577d5a28c96aaea6fc60baa758ab62b9baaae
3121kz <zelinsky.erik17@gmail.com>
2023-05-31 23:50:45 +0200

    Changing players, putting x and o, full board

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import "./App.css";
 import { useState } from "react";
 import Square from "./components/Square";
 
-
 function App() {
-  const x = <img src={Cross} alt="Cross" />
-  const o = 
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState();
-  const chooseSquare = (square) => {};
+  const [player, setPlayer] = useState("X");
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val, idx) => {
+        if (idx === square && val === "") {
+          return player;
+        }
+
+        return val;
+      })
+    );
+
+    if (player === "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  };
 
   return (
     <div className="App">
@@ -33,8 +46,47 @@ function App() {
             }}
           />
         </div>
-        <div className="row"> {x}</div>
-        <div className="row"></div>
+        <div className="row">
+          {" "}
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />{" "}
+        </div>
       </div>
     </div>
   );
